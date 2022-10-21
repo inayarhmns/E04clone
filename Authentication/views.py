580c46458937e8b2f3ce43a5a5073c5de3e36732
TehGaa <syosuasaputra1010@gmail.com>
2022-10-21 10:35:37 +0700

    Update view

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 # Create your views here.
-def show_authentication(request):
-    return render(request, )
+
+@login_required(login_url = '/Authentication/')
+def register_user(request):
+    return render(request, 'register.html')
+
+def login_user(request):
+    return render(request, 'login.html')
