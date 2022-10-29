c4ac8337df7a3b16097ba8b2367b71982fa7e6c9
inayarhmns <90492681+inayarhmns@users.noreply.github.com>
2022-10-29 15:38:40 +0700

    frontend profile page

--- profilePage/views.py
+++ profilePage/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='../Authentication/login/<str:status>/')
 def lihatProfile(request):
     context = {}
     return render(request, "lihatProfile.html", context)
+
 
 def editProfile(request):
     context = {}
