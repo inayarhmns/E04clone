72f83544be35ce93af4eed62eaf63683fdad2c58
muhammad.nafriel <muhammad.nafriel@gmail.com>
2022-11-02 22:24:20 +0700

    bismillah yaallah

--- Authentication/models.py
+++ Authentication/models.py
@@ -9,7 +9,7 @@ class Pengunjung(models.Model):
         laki_laki = 'LK', 'Male'
         perempuan = 'PP', 'Female'
 
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, default = None)
     jenis_kelamin = models.CharField(max_length = 2, choices = JenisKelamin.choices, default = JenisKelamin.laki_laki)
     kontak = models.CharField(max_length = 13, blank = True)
     alamat = models.CharField(max_length = 100, blank = True)
