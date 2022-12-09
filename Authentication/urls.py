761a83145d742f38a10d2b79f0b239e8fdcc9e96
TehGaa <syosuasaputra1010@gmail.com>
2022-12-09 10:00:39 +0700

    Update lagi

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from Authentication.views import login_user, register_user, logout_user, get_pengunjung
+from Authentication.views import login_user, register_user, logout_user, get_pengunjung, login_flutter
 
 app_name = 'Authentication'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/<str:status>/', login_user, name = 'login-user'),
     path('register/<str:status>', register_user, name = 'register-user'),
     path('logout/', logout_user, name = 'logout-user'),
-    path('json/', get_pengunjung, name = 'json')
+    path('json/', get_pengunjung, name = 'json'),
+    path('login_flutter', login_flutter, name = 'login_flutter'),
 ]
