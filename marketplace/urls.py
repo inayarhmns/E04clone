a632d85a66266f6a98f53ef9e625c762dd576fc7
TehGaa <syosuasaputra1010@gmail.com>
2022-12-12 09:30:30 +0700

    Update marketplace for flutter

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -6,8 +6,6 @@ app_name = 'marketplace'
 
 urlpatterns = [
     path('', show_marketplace, name='show_marketplace'),
-    path('form', form_marketplace, name='form_marketplace')
-    
-
-
+    path('form', form_marketplace, name='form_marketplace'),
+    path('beli_flutter', beli_flutter, name='beli_flutter'),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,7 @@
 from marketplace.forms import ShopForm
 from Authentication.models import Pengunjung
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def show_marketplace(request):
@@ -26,3 +26,18 @@ def form_marketplace(request):
     form = ShopForm()
     context = {"form": form}
     return render(request, "marketplaceform.html", context)
+
+def beli_flutter(request):
+    if request.method == "POST":
+        form = ShopForm({
+            "size": request.POST.get('size'),
+            "payment_method": request.POST.get('payment_method'),
+            "shipping_method": request.POST.get('shipping_method')
+            })   
+        obj = form.save(commit = False)
+        obj.nama_baju = request.POST.get('nama_baju')
+        obj.pengunjung = Pengunjung.objects.get(user = request.user)
+        obj.save()
+        return JsonResponse({"status": True, "message": "Purchase has been successful!"}, status = 200)
+    else:
+        return JsonResponse({"status": False, "message": "Purchase Failed"}, status = 200)
